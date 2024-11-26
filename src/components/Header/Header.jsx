f1f220924fe8519b210a88e20987a8ee5be0206e
Dmytro Klimchuk <d664433334@gmail.com>
2024-11-26 20:04:28 +0200

    btn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import s from "./Header.module.css";
 import Logo from "../../assets/logo.png";
 import { NavLink } from "react-router-dom";
-// import clsx from "clsx";
-
-// const buildLinkClass = ({ isActive }) => {
-//   return clsx(s.link, isActive && s.active);
-// };
+import { locales } from "../../locales"; // Імпортуйте тексти
+import { useState } from "react";
 
 const Header = () => {
+  const [language, setLanguage] = useState("ua"); // Стан для мови
+
+  // Функція для перемикання мови
+  const toggleLanguage = () => {
+    setLanguage((prevLang) => (prevLang === "ua" ? "en" : "ua"));
+  };
+
   return (
     <div className={s.container}>
       <header className={s.header}>
@@ -15,13 +19,17 @@ const Header = () => {
         <div>
           <nav className={s.nav}>
             <div className={s.list}>
-              <NavLink className={s.item}>ГОЛОВНА</NavLink>
+              <NavLink className={s.item}>{locales[language].home}</NavLink>
             </div>
             <div className={s.list}>
-              <NavLink className={s.item}>ПРО НАС</NavLink>
+              <NavLink className={s.item}>{locales[language].about}</NavLink>
             </div>
           </nav>
         </div>
+        {/* Кнопка для перемикання мови */}
+        <button className={s.btn} onClick={toggleLanguage}>
+          {language === "ua" ? "EN" : "UA"}
+        </button>
       </header>
     </div>
   );
