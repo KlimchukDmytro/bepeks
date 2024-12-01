5720d273f8d3e349d6b1d9ee0bcccd53ce416e5c
Dmytro Klimchuk <d664433334@gmail.com>
2024-12-01 20:56:30 +0200

    services done

--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -1,27 +1,31 @@
 import s from "./OurServices.module.css";
 import { locales } from "../../locales";
+import bepeks_services from "../../assets/bepeks_services.jpg";
 
 const OurServices = ({ language }) => {
   return (
     <section className={s.container}>
       <div className={s.containerDiv}>
         <h2 className={s.title}>{locales[language].ourServices}</h2>
-        <ul className={s.list}>
-          {locales[language].services.map((service, index) => (
-            <li key={index} className={s.item}>
-              {service.title}
-              {service.subItems && (
-                <ul className={s.itemList}>
-                  {service.subItems.map((subItem, subIndex) => (
-                    <li key={subIndex} className={s.itemItem}>
-                      {subItem}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </li>
-          ))}
-        </ul>
+        <div className={s.containerFlex}>
+          <img className={s.img} src={bepeks_services} alt="bepeks_services" />
+          <ul className={s.list}>
+            {locales[language].services.map((service, index) => (
+              <li key={index} className={s.item}>
+                {service.title}
+                {service.subItems && (
+                  <ul className={s.itemList}>
+                    {service.subItems.map((subItem, subIndex) => (
+                      <li key={subIndex} className={s.itemItem}>
+                        {subItem}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
