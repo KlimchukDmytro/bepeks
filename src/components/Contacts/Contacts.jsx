ffa288410a4991f584b6cab8df742b1e7bb5df08
Dmytro Klimchuk <d664433334@gmail.com>
2024-12-08 20:30:16 +0200

    color

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,5 +1,7 @@
 import s from "./Contacts.module.css";
 import { locales } from "../../locales";
+import officeImage from "../../assets/Office.jpg";
+import officeImg from "../../assets/Office2.jpg";
 
 const Contacts = ({ language }) => {
   const localization = locales[language] || locales["ua"];
@@ -7,19 +9,21 @@ const Contacts = ({ language }) => {
   return (
     <section className={s.container}>
       <div className={s.containerDiv}>
+        <img className={s.jpg} src={officeImage} alt="Office" />
         <h2 className={s.title}>{localization.contacts}</h2>
         <p>{localization.contactText}</p>
         <address>
           <ul className={s.list}>
             <li className={s.item}>
               {localization.phone}:
-              <a href="tel:+38067"> +38(067)тут буде ваш номер</a>
+              <a href="tel:+380673526577"> +380 (67) 352 65 77</a>
             </li>
             <li className={s.item}>
               {localization.email}:
               <a href="mailto:info@bepeks.com"> info@bepeks.com</a>
             </li>
           </ul>
+          <img className={s.jpg} src={officeImg} alt="Office2" />
         </address>
       </div>
     </section>
